6fb59986d2130d61aae7809f2345d77ca992b041
Aminat Ahmed <oahmedaminat@gmail.com>
2023-01-31 21:49:59 +0100

    updates

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -2,59 +2,45 @@ import React from "react";
 
 const Invoice = () => {
   return (
-    <div className="m-auto">
+    <div className="invoice text-center m-16">
       <h1 className="text-black text-3xl text-center">Complete your Payment</h1>
 
-      <div className="form justify-center">
-        <form className="flex flex-col justify-center">
-          <label>
-            Name
-          </label>
+      <div className="form">
+        <form className="flex flex-col align-middle text-center justify-center">
+          <label>Name</label>
           <input type="text" name="name" placeholder="Enter your name" />
           <label>
             Email
             <p>The purchase receipt would be sent to this address</p>
-            
           </label>
           <input
-              type="email"
-              name="email"
-              placeholder="Enter your email address"
-              required
-            />
-          <label>
-            Address 1
-            
-          </label>
+            type="email"
+            name="email"
+            placeholder="Enter your email address"
+            required
+          />
+          <label>Address 1</label>
           <input
-              type="text"
-              name="address"
-              placeholder="The address of the user goes here"
-            />
-          <label>
-            Address2
-            <input type="text" name="address" placeholder="and here" />
-          </label>
-          <br></br>
-          <div className="row">
-            <label>
-              Local Government
-              <input
-                type="text"
-                name="lga"
-                placeholder="Enter your local government area"
-              />
-            </label>
-            <label className="state">
-              State
-              <select value="state">
-                <option value="State">State</option>
-                <option value="Lagos">Lagos</option>
-                <option value="Ibadan">Ibadan</option>
-                <option value="portharcourt">Port Harcourt</option>
-              </select>
-            </label>
-          </div>
+            type="text"
+            name="address"
+            placeholder="The address of the user goes here"
+          />
+          <label>Address2</label>
+          <input type="text" name="address" placeholder="and here" />
+
+          <label>Local Government</label>
+          <input
+            type="text"
+            name="lga"
+            placeholder="Enter your local government area"
+          />
+          <label className="state">State</label>
+          <select value="state">
+            <option value="State">State</option>
+            <option value="Lagos">Lagos</option>
+            <option value="Ibadan">Ibadan</option>
+            <option value="portharcourt">Port Harcourt</option>
+          </select>
           <div className="buttons">
             <button className="submit" type="submit">
               Next
